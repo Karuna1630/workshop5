a10bd6ace21987a504cfad7f69e0da05c6e2b09f
Karuna1630 <karunagiri481@gmail.com>
2025-11-24 11:25:11 +0545

    task 1 completed

---
+++ workshop5/BankAccount.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace workshop5
+{
+    internal class BankAccount
+    {
+        private string accountNumber;
+        private double balance;
+
+         public string AccountNumber
+         {
+            get { return this.accountNumber; }
+         }
+        public double Balance
+        {
+            get { return this.balance; }
+            private set
+            {
+                if (value <= 0)
+                {
+                     Console.WriteLine("Balance must be positive.");
+                }
+                else
+                {
+                    this.balance = value;
+                }
+            }
+        } 
+        public BankAccount(string accountNumber, double initialBalance)
+        {
+            this.accountNumber = accountNumber;
+            if(initialBalance <= 0)
+            {
+                Console.WriteLine("Initial balance must be positive. Setting balance to 0.");
+                this.balance = 0;
+            }
+            else
+            {
+                this.balance = initialBalance;
+            }
+        }
+
+        public void Deposit(double amount)
+        {
+            if (amount >= 0)
+            {
+                this.Balance += amount;
+                Console.WriteLine($"Deposited: {amount}");
+            }
+            else
+            {
+               
+                Console.WriteLine($"Deposited must be greater than 0");
+            }
+        }
+
+
+        public void Withdraw(double amount)
+        {
+            if (amount <= 0)
+            {
+                Console.WriteLine("Withdraw amount must be greater than 0.");
+            }
+            else if (amount > this.Balance)
+            {
+                Console.WriteLine("Insufficient funds.");
+            }
+            else
+            {
+                this.Balance -= amount;
+                Console.WriteLine($"Withdrew: {amount}");
+            }
+        }
+    }
+
+
+
+   
+}
--- workshop5/Program.cs
+++ workshop5/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            BankAccount account = new BankAccount("123456", 1000);
+
+          
+            Console.WriteLine("Account Number:" + account.AccountNumber);
+
+            account.Deposit (1111.1); 
+            account.Withdraw(500);
+            Console.WriteLine("Remaining Balance: " + account.Balance);
         }
     }
 }
